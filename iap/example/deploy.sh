519dd769022e1f937ad291f891ac51779d70951d
Evan Jones <ej@evanjones.ca>
2019-08-25 17:26:11 -0400

    iap: Add headers to the demo page; use go112; remove secret name
    Output the IAP magic headers in the example page so they can be
    inspected.
    
    None of these files contain secrets; Rename them to clarify.

--- iap/example/deploy.sh
+++ iap/example/deploy.sh
@@ -4,4 +4,4 @@ set -euf -o pipefail
 # https://stackoverflow.com/a/246128
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-~/google-cloud-sdk/bin/gcloud app deploy --project=goiap-demo --promote "${DIR}/app_secret.yaml"
+~/google-cloud-sdk/bin/gcloud app deploy --project=goiap-demo --promote "${DIR}/app.yaml"

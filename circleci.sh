c65b8e85761213895f5c8ebbd570a013007e3ac1
Evan Jones <ej@evanjones.ca>
2019-10-22 21:33:20 -0400

    Add Go modules; Upgrade to Go 1.13.3 (#23)

--- circleci.sh
+++ circleci.sh
@@ -4,14 +4,21 @@
 set -euf -o pipefail
 
 # Get dependencies: TODO: Use dep or modules
-go get ./...
-
-go test -race ./...
+go test -mod=readonly -race ./...
 
 # go test only checks some vet warnings; check all
-go vet ./...
+go vet -mod=readonly ./...
 
-go get -u golang.org/x/lint/golint
+go get -mod=readonly golang.org/x/lint/golint
 golint --set_exit_status ./...
 
 diff -u <(echo -n) <(gofmt -d .)
+
+# require that we use go mod tidy. TODO: there must be as easier way
+go mod tidy
+CHANGED=$(git status --porcelain --untracked-files=no)
+if [ -n "${CHANGED}" ]; then
+    echo "ERROR files were changed:" > /dev/stderr
+    echo "$CHANGED" > /dev/stderr
+    exit 10
+fi
---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/evanj/googlesignin
+
+go 1.13
+
+require (
+	cloud.google.com/go v0.47.0 // indirect
+	github.com/stretchr/testify v1.4.0 // indirect
+	golang.org/x/crypto v0.0.0-20191011191535-87dc89f01550 // indirect
+	golang.org/x/net v0.0.0-20191021144547-ec77196f6094 // indirect
+	golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45
+	google.golang.org/appengine v1.6.5 // indirect
+	gopkg.in/square/go-jose.v2 v2.3.1
+)
